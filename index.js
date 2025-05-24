47330b2ade2453305ff48af8a91eea79ca2bb46a
Alexander Maniateas <89802016+Alexman39@users.noreply.github.com>
2025-05-24 18:09:48 +0300

    Added articles, updated image carousel and much more.

--- index.js
+++ index.js
@@ -10,3 +10,14 @@
     menuCloseButton.addEventListener('click', () => {
     menuContent.classList.remove('show');
 });
+
+    window.addEventListener('DOMContentLoaded', () => {
+        const track = document.querySelector('.carousel-track');
+        const images = Array.from(track.children);
+
+        // Duplicate images for seamless scrolling
+        images.forEach(img => {
+            const clone = img.cloneNode(true);
+            track.appendChild(clone);
+        });
+    });
